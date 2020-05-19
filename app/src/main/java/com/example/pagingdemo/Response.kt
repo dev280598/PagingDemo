b8b70c035ed5b9a2263087c2e66bedd376298114
Lê Văn Trần Đại <levantrandai.280598@gmail.com>
2020-05-19 11:03:31 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pagingdemo/Response.kt
@@ -0,0 +1,12 @@
+package com.example.pagingdemo
+
+import com.google.gson.annotations.SerializedName
+
+data class Response(
+        @SerializedName("articles") val news: List<News>
+)
+
+data class News(
+        val title: String,
+        @SerializedName("urlToImage") val image: String
+)
---
+++ app/src/main/java/com/example/pagingdemo/State.kt
@@ -0,0 +1,5 @@
+package com.example.pagingdemo
+
+enum class State {
+    DONE, LOADING, ERROR
+}
